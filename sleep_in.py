698bd289f2182efe3ea0aeead0eab6334eb73fd7
Vu Pham <vdpham326@gmail.com>
2020-06-22 22:25:30 -0700

    adding first exercise

---
+++ sleep_in.py
@@ -0,0 +1,17 @@
+# The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. 
+# We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
+
+
+def sleep_in(weekday, vacation):
+    if not weekday or vacation:
+        return True
+    elif weekday and not vacation:
+        return False
+    elif not weekday and vacation:
+        return True
+    
+
+
+print(sleep_in(False, False)) #→ True
+print(sleep_in(True, False)) #→ False
+print(sleep_in(False, True))# → True
